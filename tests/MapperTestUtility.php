f979757304cdb0caec4b5c702f8ec987f52eb674
Jalpreet Singh Nanda (imjalpreet) <jalpreetnanda@gmail.com>
2016-08-17 01:35:02 +0530

    Added test for Retrieve method in RetrieveMetadata class

--- tests/MapperTestUtility.php
+++ tests/MapperTestUtility.php
@@ -1,12 +1,23 @@
 <?php
 /**
- * ownCloud - mediametadata
+ * ownCloud - App Framework
  *
- * This file is licensed under the Affero General Public License version 3 or
- * later. See the COPYING file.
+ * @author Bernhard Posselt
+ * @copyright 2012 Bernhard Posselt dev@bernhard-posselt.com
  *
- * @author Jalpreet Singh Nanda (:imjalpreet) <jalpreetnanda@gmail.com>
- * @copyright Jalpreet Singh Nanda (:imjalpreet) 2016
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU AFFERO GENERAL PUBLIC LICENSE
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU AFFERO GENERAL PUBLIC LICENSE for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public
+ * License along with this library.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 namespace OCA\MediaMetadata\Tests;
@@ -36,7 +47,7 @@ abstract class MapperTestUtility extends TestCase {
 			->disableOriginalConstructor()
 			->getMock();
 
-		$this->query = $this->getMock('\PDOStatement');
+		$this->query = $this->getMockBuilder('\PDOStatement')->getMock();
 		$this->queryAt = 0;
 		$this->prepareAt = 0;
 		$this->iterators = [];
--- tests/unit/Services/RetrieveMetadataTest.php
+++ tests/unit/Services/RetrieveMetadataTest.php
@@ -11,7 +11,6 @@
 
 namespace OCA\MediaMetadata\Services;
 
-use OCA\MediaMetadata\AppInfo\Application;
 use OCA\MediaMetadata\Tests\MapperTestUtility;
 
 
@@ -25,58 +24,31 @@ class RetrieveMetadataTest extends MapperTestUtility {
 	 * @var \OCA\MediaMetadata\Services\RetrieveMetadata
 	 */
 	protected $retrieveData;
-	/**
-	 * @var \PHPUnit_Framework_MockObject_MockObject|\OCP\IDBConnection
-	 */
-	protected $dbConnection;
-	/**
-	 * @var $entity
-	 */
-	public static $entity = null;
-
-	public static function init() {
-		self::$entity = new ImageDimension();
-
-		self::$entity->setImageHeight(100);
-		self::$entity->setImageWidth(100);
-		self::$entity->setDateCreated('2016-04-29');
-		self::$entity->setGpsLatitude(78.21);
-		self::$entity->setGpsLongitude(27.31);
-	}
 
 	protected function setUp() {
 		parent::setUp();
 
-		self::init();
-
-		$app = new Application();
-		$container = $app->getContainer();
-
-		$this->dbConnection = $this->getMockBuilder(
-			'\OCP\IDBConnection')
-			->disableOriginalConstructor()
-			->getMock();
-
 		$this->retrieveData = new RetrieveMetadata(
-			$container->query('ServerContainer')->getDb()
+			$this->db
 		);
 	}
 
 	public function testRetrieve() {
-		$expectedResult = array();
 
 		$fileList = array(260495);
 		
-		$expectedResult[strval(260495)] = array(
-			'imageHeight' => 100,
-			'imageWidth'  => 100,
-			'dateCreated' => '2016-04-29',
-			'gpsLongitude' => 78.21,
-			'gpsLatitude' => 27.31
+		$expectedResult = array(
+			'260495' => array(
+				'imageHeight' => 100,
+				'imageWidth'  => 100,
+				'dateCreated' => '2016-04-29',
+				'gpsLongitude' => 78.21,
+				'gpsLatitude' => 27.31
+			)
 		);
 
 		$imageID = 260495;
-		$sql = 'SELECT * FROM `*PREFIX*mediametadata` WHERE `image_id` = ?';
+		$sql = 'SELECT * FROM *PREFIX*mediametadata WHERE image_id = ?';
 		$params = [$imageID];
 		$row = [
 			'image_height' => 100,
@@ -85,6 +57,8 @@ class RetrieveMetadataTest extends MapperTestUtility {
 			'gps_latitude' => 27.31,
 			'gps_longitude' => 78.21
 		];
+
+
 		$this->setMapperResult($sql, $params, [$row]);
 
 		$this->assertEquals($this->retrieveData->retrieve($fileList), $expectedResult);
